944bf632d05666188e1aaabc95864e1361307aa6
zhgaocs <2049878696@qq.com>
2024-04-10 12:05:54 +0800

    Add the definition of TCPClient::connect

--- src/tcpclient.cpp
+++ src/tcpclient.cpp
@@ -2,4 +2,16 @@
 
 bool TCPClient::connect(const char *ip, int port)
 {
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, ip, &serverAddr.sin_addr) <= 0)
+        return false;
+
+    if (::connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+        return false;
+
+    return true;
 }
