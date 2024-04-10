9323e9d31cfcaff0e345d099d74354a2ea1accb1
zhgaocs <2049878697@qq.com>
2024-04-10 11:15:32 +0800

    Add function definition

--- src/tcpsocket.cpp
+++ src/tcpsocket.cpp
@@ -2,9 +2,29 @@
 
 TCPSocket::TCPSocket() : sockfd(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP))
 {
+    if (sockfd < 0)
+        throw std::runtime_error("Failed to create socket");
 }
 
 TCPSocket::~TCPSocket()
 {
-    ::close(sockfd);
+    close(sockfd);
+}
+
+ssize_t TCPSocket::read(char *buf, size_t bufsize)
+{
+    ssize_t has_read = ::read(sockfd, buf, bufsize);
+    if (has_read == -1)
+        throw std::runtime_error("Read error");
+
+    return has_read;
+}
+
+ssize_t TCPSocket::write(const char *msg)
+{
+    ssize_t has_written = ::write(sockfd, msg, strlen(msg));
+    if (has_written == -1)
+        throw std::runtime_error("Write error");
+
+    return has_written;
 }
