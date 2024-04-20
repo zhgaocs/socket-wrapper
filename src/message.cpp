82d68c8c2f1fe214b58f0adde36f4d6dc3b93217
zhgaocs <2049878697@qq.com>
2024-04-20 18:46:53 +0800

    Refactor serialization and deserialization funcs

--- src/message.cpp
+++ src/message.cpp
@@ -12,45 +12,54 @@ Message::~Message()
         delete[] data;
 }
 
-Message &Message::setDestPort(uint16_t port)
+Message &Message::setMsgType(MessageType type)
+{
+    header.msg_type = type;
+    return *this;
+}
+
+Message &Message::setDstPort(uint16_t port)
 {
     header.dst_port = htons(port);
     return *this;
 }
 
-Message &Message::setSourcePort(uint16_t port)
+Message &Message::setSrcPort(uint16_t port)
 {
     header.src_port = htons(port);
     return *this;
 }
 
-Message &Message::setData(const char *data)
+Message &Message::setData(const char *buf, size_t bufsize)
 {
-    if (this->data)
-        delete[] this->data;
+    if (data)
+    {
+        delete[] data;
+        header.data_len = 0;
+    }
 
-    size_t data_len = strlen(data);
-    this->data = new char[data_len + 1];
-    strncpy(this->data, data, data_len);
-    this->data[data_len] = '\0';
+    if (!bufsize || !buf)
+        return *this;
 
-    header.length = data_len + 1; // include '\0'
+    data = new char[bufsize];
+    memcpy(data, buf, /* sizeof(char) * */ bufsize);
+    header.data_len = bufsize;
 
     return *this;
 }
 
-int serialize(const Message &msg, char *buf, size_t bufsize)
+int serialize(char *buf, size_t bufsize, const Message &msg)
 {
-    if (bufsize < sizeof(Header) + msg.header.length)
-        return -1;
+    if (bufsize < sizeof(Header) + msg.header.data_len)
+        return 0;
 
     memcpy(buf, &msg.header, sizeof(Header));
-    memcpy(buf + sizeof(Header), msg.data, msg.header.length);
+    memcpy(buf + sizeof(Header), msg.data, /* sizeof(char) * */ msg.header.data_len);
 
-    return sizeof(Header) + msg.header.length;
+    return sizeof(Header) + msg.header.data_len;
 }
 
-int deserialize(const char *buf, size_t bufsize, Message &msg)
+int deserialize(Message &msg, const char *buf, size_t bufsize)
 {
     if (bufsize < sizeof(Header))
         return 0;
@@ -58,19 +67,27 @@ int deserialize(const char *buf, size_t bufsize, Message &msg)
     Header header;
     memcpy(&header, buf, sizeof(Header));
 
-    int rest_len = bufsize - sizeof(Header);
-    if (rest_len < header.length)
+    if (bufsize < sizeof(Header) + header.data_len)
         return 0;
 
-    char *data = new (std::nothrow) char[header.length];
-    if (!data)
-        return -1;
-    memcpy(data, buf + sizeof(Header), header.length);
+    if (header.data_len)
+    {
+        char *data = new (std::nothrow) char[header.data_len];
+        if (!data)
+            return -1;
+        memcpy(data, buf + sizeof(Header), /* sizeof(char) * */ header.data_len);
+
+        if (msg.data)
+            delete[] msg.data;
+        msg.data = data;
+    }
+    else if (msg.data)
+    {
+        delete[] msg.data;
+        msg.data = nullptr;
+    }
 
     msg.header = std::move(header);
-    if (!msg.data)
-        delete[] msg.data;
-    msg.data = data;
 
-    return sizeof(Header) + msg.header.length;
+    return sizeof(Header) + msg.header.data_len;
 }
