1139d2998bb586c98ec0378504b231b73c511c38
zhgaocs <2049878697@qq.com>
2024-04-20 18:32:39 +0800

    Add MessageType enum struct

--- include/message.h
+++ include/message.h
@@ -7,9 +7,16 @@
 #include <stdexcept>
 #include <utility>
 
+enum struct MessageType : uint16_t
+{
+    PORT_INFO,
+    REG_MSG
+};
+
 struct Header
 {
-    uint32_t length;   /* contains '\0' in data*/
+    MessageType msg_type;
+    uint16_t data_len;
     uint16_t src_port; /* port in network byte order */
     uint16_t dst_port; /* port in network byte order */
 };
@@ -21,15 +28,16 @@ struct Message
     Message(const Message &) = delete;
     Message &operator=(const Message &) = delete;
 
-    Message &setDestPort(uint16_t port);
-    Message &setSourcePort(uint16_t port);
-    Message &setData(const char *data);
+    Message &setMsgType(MessageType type);
+    Message &setDstPort(uint16_t port);
+    Message &setSrcPort(uint16_t port);
+    Message &setData(const char *buf, size_t bufsize);
 
     Header header;
     char *data;
 };
 
-int serialize(const Message &msg, char *buf, size_t bufsize);
-int deserialize(const char *buf, size_t bufsize, Message &msg);
+int serialize(char *buf, size_t bufsize, const Message &msg);
+int deserialize(Message &msg, const char *buf, size_t bufsize);
 
 #endif
