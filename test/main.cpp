d324d5cacdbb676c556af4c3dd4a4ff21c9b093b
zhgaocs <2049878696@qq.com>
2024-04-24 22:07:14 +0800

    Modify main()

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,28 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
+
+#include "tcpclient.h"
+#include "tcpserver.h"
 
 int main(int argc, char *argv[])
 {
-    
-    std::cin.get();
+    const char *servip = "127.0.0.1";
+    const uint16_t servport = 12345;
+
+    std::thread server_thread(
+        [servport]()
+        {
+            try
+            {
+                TCPServer server(servport);
+                server.run();
+            }
+            catch (const std::exception &e)
+            {
+                std::cerr << "Server error: " << e.what() << std::endl;
+            };
+        });
+
+    server_thread.detach();
 }
